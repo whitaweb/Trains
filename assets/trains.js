b04e8b0acc585b2c812a62886bbf6631503aaf9b
whitaweb <26194993+whitaweb@users.noreply.github.com>
2017-10-12 14:46:36 -0500

    new files

--- assets/trains.js
+++ assets/trains.js
@@ -12,15 +12,18 @@
    // Create a variable to reference the database
    var database = firebase.database();
 
-      // Firebase Variables
+      
+
+      $('#submit').on("click", function(){
+        event.preventDefault();
+
+        // Firebase Variables
 
       var trainName = $('#name').val().trim();
       var destination = $('#dest').val().trim();
       var trainTime = $('#time').val().trim();
       var frequency= $('#frequencymin').val().trim();
 
-      $('#submit').on("click", function(){
-        event.preventDefault();
 
     console.log('all:', trainName, destination, trainTime, frequency);
 
@@ -37,14 +40,20 @@
 
       database.ref().push(trainData);
 
+      // Logs everything to console
+  console.log(trainData.trainName);
+  console.log(trainData.destination);
+  console.log(trainData.trainTime);
+  console.log(trainData.frequency );
+
         // Add Alert or Modal
         // Here.......
 
         // Clears all of the text-boxes
-  // $("#name").val("");
-  // $("#dest").val("");
-  // $("#time").val("");
-  // $("#frequencymin").val("");
+  $("#name").val("");
+  $("#dest").val("");
+  $("#time").val("");
+  $("#frequencymin").val("");
 
 });
 
@@ -66,10 +75,13 @@
         console.log(newTime);
         console.log(newFrequency);
        
-        // $("#trainname").html(snapshot.val().trainName);
-        // $("#destination").html(snapshot.val().destination);
-        // $("#frequency").html(snapshot.val().frequency);
-        // $("#nextarrival").html(snapshot.val().trainTime);
+        $("#trainname").html(snapshot.val().trainName);
+        $("#destination").html(snapshot.val().destination);
+        $("#frequency").html(snapshot.val().frequency);
+        $("#nextarrival").html(snapshot.val().trainTime);
+
+      var minutesAway = newTime + newFrequency;
+      console.log("Next Arrival", minutesAway);
 
          // Add each train's data into the table
   $("#tablebody > tbody").append("<tr><td>" + newTrain + "</td><td>" + newDestination + "</td><td>" +
